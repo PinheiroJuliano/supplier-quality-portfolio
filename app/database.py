12c294c0d782a4c4e42e8886d27a2de316984540
JULIANO <pisjuliano@gmail.com>
2026-01-21 15:45:50 -0300

    adaptação ao ambiente no db

--- app/database.py
+++ app/database.py
@@ -1,6 +1,15 @@
+import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = "sqlite:////tmp/suppliers.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Se estiver no Render, usa /tmp
+if os.getenv("RENDER"):
+    DATABASE_PATH = "/tmp/suppliers.db"
+else:
+    DATABASE_PATH = os.path.join(BASE_DIR, "suppliers.db")
+
+DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
 
 engine = create_engine(
     DATABASE_URL,
